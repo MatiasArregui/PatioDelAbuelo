c236d2e470db60abf51c2a1cb2a716bb1a7dd522
MatiasAshegui <matiasarregui2@gmail.com>
2024-06-07 22:22:41 -0300

    Acomodando models

--- patioAbuelo/miAplicacion/admin.py
+++ patioAbuelo/miAplicacion/admin.py
@@ -1,27 +1,64 @@
 from django.contrib import admin
-from .models import Plato, PlatoOrden, Orden, Mesa, Factura, Cliente
+from .models import Plato, PlatoOrden, Orden, Mesa, Factura, Cliente, FacturaOrden, Bebida, Postre, BebidaOrden, PostreOrden
 # Register your models here.
-admin.site.register(Plato)
 admin.site.register(Mesa)
-admin.site.register(Cliente)
-# admin.site.register(Factura)
 
-class DetalleInline(admin.TabularInline):
+# DETALLES DE BEBIDA -------------------------------------->
+class BebidaDetalle(admin.ModelAdmin):
+    list_display = ("id", "nombre", "precio", "cantidad")
+    
+admin.site.register(Bebida, BebidaDetalle)
+
+# DETALLES DE POSTRE -------------------------------------->
+class PostreDetalle(admin.ModelAdmin):
+    list_display = ("id", "nombre", "precio", "cantidad")
+    
+admin.site.register(Postre, PostreDetalle)
+
+# DETALLES DE PLATO -------------------------------------->
+class PlatoDetalle(admin.ModelAdmin):
+    list_display = ("id", "nombre", "precio")
+    
+admin.site.register(Plato, PlatoDetalle)
+
+# DETALLES DE CLIENTE -------------------------------------->
+class ClienteDetalle(admin.ModelAdmin):
+    list_display = ("id", "nombre", "telefono")
+
+admin.site.register(Cliente, ClienteDetalle)
+
+
+# INLINES DE ORDEN Y FACTURA ----------------------------------------->
+
+# INLINE DETALLE DE ORDEN ---------------------------->
+class OrdenDetalleInline(admin.TabularInline):
     model = PlatoOrden
 
+class PostreDetalleInline(admin.TabularInline):
+    model = PostreOrden
+    
+class BebidaDetalleInline(admin.TabularInline):
+    model = BebidaOrden
 
 class OrdenAdmin(admin.ModelAdmin):
     inlines = [
-        DetalleInline,
+        OrdenDetalleInline,
+        BebidaDetalleInline,
+        PostreDetalleInline
     ]
+    list_display = ("id", "id_mesa")
+    
 admin.site.register(Orden, OrdenAdmin)
 
+# INLINE DETALLE DE FACTURA ---------------------------->    
 class OrdenInline(admin.TabularInline):
-    model = Orden
-
+    model = FacturaOrden
 
 class FacturaAdmin(admin.ModelAdmin):
     inlines = [
         OrdenInline,
     ]
+    list_display = ("id", "fecha", "Total")
+
 admin.site.register(Factura, FacturaAdmin)
+
--- patioAbuelo/miAplicacion/models.py
+++ patioAbuelo/miAplicacion/models.py
@@ -1,57 +1,106 @@
 from django.db import models
 
 # Create your models here.
-
-
 # MODELO DE PLATO ------------------------------------>
 class Plato(models.Model):
-    nombre = models.CharField(max_length=255)
-    precio = models.IntegerField()
+    nombre = models.CharField(max_length=120)
+    precio = models.DecimalField(max_digits=15, decimal_places=2)
+    
+    def __str__(self) -> str:
+        return self.nombre + " " + str(self.precio)
+    
+# MODELO DE BEBIDAS ------------------------------------>
+class Bebida(models.Model):
+    nombre = models.CharField(max_length=120)
+    precio = models.DecimalField(max_digits=15, decimal_places=2)
+    cantidad = models.IntegerField()
+    
+    def __str__(self) -> str:
+        return self.nombre + " " + str(self.precio)
+    
+# MODELO DE BEBIDAS ------------------------------------>
+class Postre(models.Model):
+    nombre = models.CharField(max_length=120)
+    precio = models.DecimalField(max_digits=15, decimal_places=2)
+    cantidad = models.IntegerField()
     
     def __str__(self) -> str:
         return self.nombre + " " + str(self.precio)
     
 # MODELO DE MESA -----------------------------------------> 
 class Mesa(models.Model):
-    nombre = models.CharField(max_length=255)
+    nombre = models.CharField(max_length=120)
+    estado = models.BooleanField(default=False, null=True, blank=True)
     
     def __str__(self) -> str:
-        return self.nombre   
+        return str(self.pk) + " " + self.nombre
 
 # MODELO DE CLIENTE ----------------------------------------->
 class Cliente(models.Model):
-    nombre = models.CharField(max_length=255)
-    telefono = models.CharField(max_length=30)
+    nombre = models.CharField(max_length=120)
+    telefono = models.CharField(max_length=30, null=True, blank=True)
+    direccion = models.CharField(max_length=120, null=True, blank=True)
     
     def __str__(self) -> str:
         return self.nombre
 
 # MODELO FACTURA ----------------------------------------------->
 class Factura(models.Model):
+    fecha = models.DateField(auto_now=True)
+    Total = models.DecimalField(max_digits=15, decimal_places=2)
     Id_cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
-    fecha = models.DateField()
-    Total = models.IntegerField()
+    id_orden = models.ManyToManyField("Orden", through="FacturaOrden")
     
     def __str__(self) -> str:
-        return self.Id_cliente.nombre + " " + str(self.fecha)
+        return str(self.pk) + " " + self.Id_cliente.nombre + " " + str(self.fecha)
 
 # MODELO DE ORDEN ----------------------------------------->
 class Orden(models.Model):
+    total = models.DecimalField(max_digits=15, decimal_places=2)
+    observacion = models.TextField(null=True, blank=True)
     id_mesa = models.ForeignKey(Mesa, on_delete=models.CASCADE)
-    id_factura = models.ForeignKey(Factura, on_delete=models.CASCADE, default=None, blank=True, null=True)
     id_plato = models.ManyToManyField(Plato, through="PlatoOrden")
-    total = models.IntegerField()
+    id_postre = models.ManyToManyField(Postre, through="PostreOrden")
+    id_bebida = models.ManyToManyField(Bebida, through="BebidaOrden")
+ 
     
     def __str__(self) -> str:
         return str(self.pk) + " " + self.id_mesa.nombre + " " + str(self.total) 
     
-
     
 # MODELO DE PLATO_ORDEN ----------------------------------------->
 class PlatoOrden(models.Model):
     id_plato = models.ForeignKey(Plato, on_delete=models.CASCADE)
     id_orden = models.ForeignKey(Orden, on_delete=models.CASCADE)
+    cantidad = models.IntegerField(default=1)
 
     def __str__(self) -> str:
         return str(self.pk)
+
+# MODELO DE POSTRE_ORDEN ----------------------------------------->
+class PostreOrden(models.Model):
+    id_postre = models.ForeignKey(Postre, on_delete=models.CASCADE)
+    id_orden = models.ForeignKey(Orden, on_delete=models.CASCADE)
+    cantidad = models.IntegerField(default=0)
+
+    def __str__(self) -> str:
+        return str(self.pk)
+    
+# MODELO DE BEBIDA_ORDEN ----------------------------------------->
+class BebidaOrden(models.Model):
+    id_bebida = models.ForeignKey(Bebida, on_delete=models.CASCADE)
+    id_orden = models.ForeignKey(Orden, on_delete=models.CASCADE)
+    cantidad = models.IntegerField(default=1)
+
+    def __str__(self) -> str:
+        return str(self.pk)
+
+# MODELO DE FACTURA_ORDEN ----------------------------------------->
+class FacturaOrden(models.Model):
+    id_orden = models.ForeignKey(Orden, on_delete=models.CASCADE)
+    id_factura = models.ForeignKey(Factura, on_delete=models.CASCADE)
+    
+    def __str__(self) -> str:
+        return str(self.pk)
+    
     
