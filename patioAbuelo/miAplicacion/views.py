3dd99f8f02b9278956dbfe215131ca75ae0fed57
matiasAshegui <matiasarregui2@gmail.com>
2024-08-15 11:52:27 -0300

    Formulario bebidas realizado

--- patioAbuelo/miAplicacion/views.py
+++ patioAbuelo/miAplicacion/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 from .models import Bebida, Plato, Postre, Cliente, Mesa, Orden, Factura
+from django.urls import reverse
 
 # Create your views here.
 def principal(request):
@@ -29,3 +30,37 @@ def listaClientes(request):
     clientes = Cliente.objects.all()
     context = {"clientes": clientes}
     return render(request, template_name="listaClientes.html", context=context)
+
+
+def bebidaModificar(request, pk):
+    bebida = Bebida.objects.get(id=pk)
+    if request.method == 'POST':
+        nombre = request.POST.get('nombre')
+        precio = request.POST.get('precio')
+        cantidad = request.POST.get('cantidad')
+
+        bebida.nombre = nombre
+        bebida.precio = precio
+        bebida.cantidad = cantidad
+        bebida.save()
+        return HttpResponseRedirect(reverse('listaBebidas'))
+    return render(request, "formularioBebidas.html", {'bebida': bebida})
+
+
+def bebidaNuevo(request):
+    if request.method == 'POST':
+        nombre = request.POST.get('nombre')
+        precio = request.POST.get('precio')
+        cantidad = request.POST.get('cantidad')
+
+        Bebida.objects.create(nombre=nombre, precio=precio, cantidad=cantidad)
+        return HttpResponseRedirect(reverse('listaBebidas'))
+    return render(request, "formularioBebidas.html")
+
+
+def bebidaBorrar(request, pk):
+    bebida = Bebida.objects.get(id=pk)
+    if request.method == 'POST':
+        bebida.delete()
+        return HttpResponseRedirect(reverse('listaBebidas'))
+    return render(request, 'bebidaConfBorrar.html', {'bebida': bebida})
