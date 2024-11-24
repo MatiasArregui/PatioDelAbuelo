f9efa807f2d59766c39a102726d523b1455c3c2e
matiasAshegui <matiasarregui2@gmail.com>
2024-11-23 22:09:46 -0300

    re-subir-4

---
+++ patioAbuelo/miAplicacion/urls.py
@@ -0,0 +1,62 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+from django.contrib.auth.views import LoginView
+from .forms import LoginForm
+from django.contrib.auth.decorators import login_required
+
+urlpatterns = [
+    #Login ----------------------------------------------------------------------------------->
+    path('', LoginView.as_view(
+        template_name='registration/login.html',
+        authentication_form=LoginForm
+    ), name='login'),
+    
+    #Pagina de inicio ------------------------------------------------------------------------>
+    path("inicio/", login_required(login_required(views.principal)), name="paginaPrincipal"),
+   
+    
+    #Ordenes -------------------------------------------------------------------------------->
+    path("ordenes/", login_required(views.listaOrdenes.as_view()), name="listaOrdenes"),
+    path("ordenes/ordenNueva/", login_required(views.ordenNuevo), name="ordenNueva"),
+    path("ordenes/ordenModif/<int:pk>/", login_required(views.ordenModificar), name="ordenModificar"),
+    path("ordenes/ordenBorrar/<int:pk>/", login_required(views.ordenBorrar), name="ordenBorrar"),
+    
+    #Facturas -------------------------------------------------------------------------------->
+    path("facturas/", login_required(views.listaFacturas.as_view()), name="listaFacturas"),
+    path("facturas/facturaNueva/", login_required(views.facturaNuevo), name="facturaNueva"),
+    path("facturas/facturaModif/<int:pk>/", login_required(views.facturaModificar), name="facturaModificar"),
+    path("facturas/facturaBorrar/<int:pk>/", login_required(views.facturaBorrar), name="facturaBorrar"),
+    
+    #clientes -------------------------------------------------------------------------------->
+    path("clientes/", login_required(views.listaClientes.as_view()), name="listaClientes"),
+    path("clientes/clienteNuevo/", login_required(views.ClienteNuevo), name="clienteNuevo"),
+    path("clientes/clienteModif/<int:pk>/", login_required(views.ClienteModif), name="clienteModificar"),
+    path("clientes/clienteBorrar/<int:pk>/", login_required(views.ClienteBorrar), name="clienteBorrar"),
+
+    #Mesas ------------------------------------------------------------------------------------->
+    path("mesas/", login_required(views.listaMesas.as_view()), name="listaMesas"),
+    path('mesas/mesaNueva/', login_required(views.mesaNueva), name='mesaNueva'),
+    path('mesas/mesaModif/<int:pk>/', login_required(views.mesaModificar), name='mesaModif'),
+    path('mesas/mesaBorrar/<int:pk>/', login_required(views.mesaBorrar), name='mesaBorrar'),
+    
+    #Carta ---------------------------------------------------------------------------------->
+    path("carta/", login_required(views.listaCarta.as_view()), name="listaCarta"),
+    path('carta/cartaNuevo/', login_required(views.cartaNuevo), name='cartaNuevo'),
+    path('carta/cartaModif/<int:pk>/', login_required(views.cartaModificar), name='cartaModif'),
+    path('carta/cartaBorrar/<int:pk>/', login_required(views.cartaBorrar), name='cartaBorrar'),
+    
+    #Mozos ---------------------------------------------------------------------------------->
+    path("mozos/", login_required(views.listaMozos.as_view()), name="listaMozos"),
+    path('mozos/mozoNuevo/', login_required(views.MozoNuevo), name='mozoNuevo'),
+    path('mozos/mozoModif/<int:pk>/', login_required(views.MozoModif), name='mozoModif'),
+    path('mozos/mozoBorrar/<int:pk>/', login_required(views.MozoBorrar), name='mozoBorrar'),
+    
+    #Cierre ---------------------------------------------------------------------------------->
+    path("cierre/", login_required(views.listaCierres.as_view()), name="listaCierres"),
+    path('cierre/cierreNuevo/', login_required(views.cierreNuevo), name='cierreNuevo'),
+    path('cierre/cierreModif/<int:pk>/', login_required(views.cierreModif), name='cierreModif'),
+    path('cierre/cierreBorrar/<int:pk>/', login_required(views.cierreBorrar), name='cierreBorrar'),
+    
+    
+]
