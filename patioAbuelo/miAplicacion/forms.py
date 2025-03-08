a25f282f42eab47ac8bbdede9ccefcb25eb90866
Matias Nicolas Arregui <matiasarregui2@gmail.com>
2025-03-08 13:08:30 -0300

    interfaz carta y login

--- patioAbuelo/miAplicacion/forms.py
+++ patioAbuelo/miAplicacion/forms.py
@@ -3,6 +3,7 @@ from .models import Mozo, Cliente, Carta, Categoria, SubCategoria, CartaOrden, O
 from django.forms import inlineformset_factory
 from django.contrib.auth.forms import AuthenticationForm
 
+
 class MozoForm(forms.ModelForm):
     class Meta:
         model = Mozo
@@ -190,16 +191,25 @@ class LoginForm(AuthenticationForm):
         label='Usuario',
         widget=forms.TextInput(attrs={
             'class': 'form-control',
-            'placeholder': 'Nombre de usuario'
+            'placeholder': 'Nombre de usuario',
+            'autocomplete': 'username' 
         })
     )
     password = forms.CharField(
         label='Contraseña',
         widget=forms.PasswordInput(attrs={
             'class': 'form-control',
-            'placeholder': 'Contraseña'
+            'placeholder': 'Contraseña',
+            'autocomplete': 'current-password' 
         })
     )
+
+    # Personalizar los mensajes de error
+    error_messages = {
+        'invalid_login': "Usuario o contraseña incorrectos. Por favor, inténtelo nuevamente.",
+        'inactive': "Esta cuenta está inactiva. Contacte al administrador para más información.",
+    }
+
 
 
 class PlatoDiaForm(forms.ModelForm):
